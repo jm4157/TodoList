cf26c7a52b26958ee1c6146d198ac5fafe3e3468
jm4157 <jm4157@rit.edu>
2024-05-27 16:45:45 -0400

    Added documentation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import {useState} from 'react';
 
+/**
+ * Author: Judah Munoz
+ * 
+ * The core element that manages todolist state.
+ * 
+ * @returns The todolist page
+ */
 export default function TodoList(){
   const [tasks, setTasks] = useState([]);
 
+  /** 
+   * Reads the task provided in the textbox, and if not null adds it to the
+   * list of tasks.
+  */
   function handleTaskAdd(){
     const addField = document.getElementById("addField");
     const taskText = addField.value;
 
     if (taskText) {
       const newTasks = [...tasks, taskText];
-      
+
       setTasks(newTasks);
       addField.value = null
     }
   }
 
+  /**
+   * Removes a task from the list.
+   * 
+   * @param {number} index The index of the task to be removed
+   */
   function handleRemove(index){
     const newTasksHead = tasks.slice(0, index);
     const newTasksTail = tasks.slice(index + 1, tasks.length);
@@ -37,6 +53,13 @@ export default function TodoList(){
 }
 
 
+/**
+ * The page element that allows a user to adds tasks to their todo 
+ * list.
+ * 
+ * @param {function} onSubmit The function that handles input
+ * @returns The Task Adder portion of the page
+ */
 function TaskAdder({onSubmit}){
   return (
     <>
@@ -49,7 +72,19 @@ function TaskAdder({onSubmit}){
 }
 
 
+/**
+ * The page element that allows a user view and remove tasks from 
+ * their todo list.
+ * 
+ * @param {Array} tasks The list of tasks
+ * @param {function} onRemove The function that handles task removal
+ * @returns The Task tracker portion of the page
+ */
 function TaskTracker({tasks, onRemove}){
+  /**
+   * Maps the list of tasks as raw text to a list of tasks as display objects
+   * and removal buttons.
+   */
   const taskButtons = tasks.map((taskText, index) => {
     return (
       <li key={index}>
