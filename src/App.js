6abe1ab6a6363f54c651bc259a0fb0eedc6bf0ae
jm4157 <jm4157@rit.edu>
2024-05-27 16:02:27 -0400

    Created an input to add tasks

--- src/App.js
+++ src/App.js
@@ -1,43 +1,29 @@
 import {useState} from 'react';
-export default function Game(){
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares){
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
+export default function TodoList(){
+  const [tasks, setTasks] = useState([]);
 
-  function jumpTo(nextMove){
-    setCurrentMove(nextMove);
-  }
+  function handleTaskAdd(){
+    const addField = document.getElementById("addField");
+    const taskText = addField.value;
 
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = "Go to move #" + move;
-    } else {
-      description = "Go to game start"
+    if (taskText) {
+      const newTasks = [...tasks, taskText];
+      setTasks(newTasks);
+      addField.value = null
+
+      console.log(tasks);
     }
-
-    return (
-      <li key={move}>
-        <button onClick = {() => jumpTo(move)}> {description} </button>
-      </li>
-    )
-  });
+  }
 
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    <div className="todo-list">
+      <div className="task-adder">
+        <TaskAdder onSubmit={handleTaskAdd} />
       </div>
-      <div className="game-info">
+      <div className="task-tracker">
         <ol>
-          {moves}
+          {/*TODO*/}
         </ol>
       </div>
     </div>
@@ -45,76 +31,14 @@ export default function Game(){
 }
 
 
-function Board({xIsNext, squares, onPlay}) {
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O")
-  }
-
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)){return;}
-
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-
-    onPlay(nextSquares);
-  }
-
+function TaskAdder({onSubmit}){
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <div className="label"> Enter Task: </div>
+      <input type="text" id="addField" ></input>
+      <br></br>
+      <button onClick={onSubmit}>Add</button>
     </>
-  );
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
+  )
 }
 
-
-function Square({value, onSquareClick}) {
-  return <button 
-    className="square"
-    onClick={onSquareClick}
-    >
-      {value}
-    </button>
-}
